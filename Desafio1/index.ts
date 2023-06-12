74fb945450564967c0c50a95258335d8da29738f
Marco <dev.marcotullio@gmail.com>
2023-06-12 12:15:27 -0300

    First Commit

---
+++ Desafio1/index.ts
@@ -0,0 +1,8 @@
+const numero1 = 10
+const numero2 = 20
+
+function soma(numero1:number, numero2:number) {
+    console.log("A soma dos números foi " + (numero1 + numero2))
+}
+
+soma(numero1,numero2)
