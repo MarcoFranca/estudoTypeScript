a97dd5865d248b9d8a29e97f5111e679ab471738
Marco <dev.marcotullio@gmail.com>
2023-06-12 11:10:11 -0300

    First Commit

---
+++ IntroTS/index.ts
@@ -0,0 +1,11 @@
+const firstName = "Marco"
+const anotherName = 1
+const x = true
+
+function greeting(name: string) {
+    console.log("olá " + name)
+}
+
+greeting(firstName)
+// greeting(anotherName);
+// greeting(x);K
